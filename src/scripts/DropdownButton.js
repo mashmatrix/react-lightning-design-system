154988d66936147d4e1b8f293824d41a236f4a1c
Mike Kruk <tamagokun@gmail.com>
2016-02-06 15:52:15 -0500

    Update DropdownButton.js
    If the component is getting unmounted at this point, no dom node will be found and .trigger will fail.

--- src/scripts/DropdownButton.js
+++ src/scripts/DropdownButton.js
@@ -64,7 +64,7 @@ export default class DropdownButton extends React.Component {
     if (!this.props.hoverPopup) {
       setTimeout(() => {
         const triggerElem = ReactDOM.findDOMNode(this.refs.trigger);
-        triggerElem.focus();
+        if (triggerElem) triggerElem.focus();
         this.setState({ opened: false });
       }, 10);
     }
