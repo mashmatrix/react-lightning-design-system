885c84dba201fe0443a0564fc7df181492ed6c47
Shinichi Tomita <shinichi.tomita@gmail.com>
2016-05-20 09:12:25 +0900

    fix icons list in examples

--- examples/client/scripts/components/IconExamples.js
+++ examples/client/scripts/components/IconExamples.js
@@ -3,17 +3,17 @@ import React from 'react';
 import { Icon } from 'react-lightning-design-system';
 
 const STANDARD_ICONS = `
-account,announcement,answer_best,answer_private,answer_public,approval,apps_admin,
-apps,article,avatar_loading,avatar,calibration,call_history,call,campaign_members,
-campaign,canvas,case_change_status,case_comment,case_email,case_log_a_call,case_transcript,
-case,client,coaching,connected_apps,contact,contract,custom,dashboard,default,document,
-drafts,email_chatter,email,empty,endorsement,environment_hub,event,feed,feedback,
-file,flow,generic_loading,goals,group_loading,groups,home,household,insights,investment_account,
-lead,link,log_a_call,marketing_actions,marketing_resources,metrics,news,note,opportunity,
-orders,people,performance,photo,poll,portal,post,pricebook,process,product,question_best,
+account,announcement,answer_best,answer_private,answer_public,approval,apps,apps_admin,
+article,avatar,avatar_loading,calibration,call,call_history,campaign,campaign_members,
+canvas,case,case_change_status,case_comment,case_email,case_log_a_call,case_transcript,
+client,coaching,connected_apps,contact,contract,custom,dashboard,default,document,
+drafts,email,email_IQ,email_chatter,empty,endorsement,environment_hub,event,feed,feedback,
+file,flow,folder,generic_loading,goals,group_loading,groups,hierarchy,home,household,insights,investment_account,
+lead,link,log_a_call,marketing_actions,metrics,news,note,opportunity,
+orders,people,performance,person_account,photo,poll,portal,post,pricebook,process,product,question_best,
 question_feed,quotes,recent,record,related_list,report,reward,scan_card,skill_entity,
-social,solution,sossession,task,task2,team_member,thanks_loading,thanks,today,topic,
-unmatched,user
+social,solution,sossession,task,task2,team_member,thanks,thanks_loading,today,topic,
+unmatched,user,work_order,work_order_item
 `
 .replace(/^\s+|\s+$/g, '').split(/[\s,]+/);
 
@@ -22,14 +22,14 @@ const CUSTOM_ICONS =
 
 const ACTION_ICONS = `
 add_contact,announcement,apex,approval,back,call,canvas,change_owner,change_record_type,
-check,clone,close,defer,delete,description,dial_in,download,edit_groups,edit_relationship,
-edit,email,fallback,filter,flow,follow,following,freeze_user,goal,google_news,join_group,
-lead_convert,leave_group,log_a_call,log_event,manage_perm_sets,map,more,new_account,
+check,clone,close,defer,delete,description,dial_in,download,edit,edit_groups,edit_relationship,
+email,fallback,filter,flow,follow,following,freeze_user,goal,google_news,info,join_group,
+lead_convert,leave_group,log_a_call,log_event,manage_perm_sets,map,more,new,new_account,
 new_campaign,new_case,new_child_case,new_contact,new_event,new_group,new_lead,new_note,
-new_notebook,new_opportunity,new_task,new,password_unlock,preview,priority,question_post_action,
-quote,record,refresh,reject,remove,reset_password,share_file,share_link,share_poll,
-share_post,share_thanks,sort,submit_for_approval,update_status,update,user_activation,
-web_link,new_custom1,new_custom2,new_custom3,new_custom4,new_custom5,new_custom6,
+new_notebook,new_opportunity,new_person_account,new_task,password_unlock,preview,priority,question_post_action,
+quote,record,refresh,reject,remove,reset_password,share,share_file,share_link,share_poll,
+share_post,share_thanks,sort,submit_for_approval,update,update_status,upload,user,user_activation,
+web_link,
 new_custom7,new_custom8,new_custom9,new_custom10,new_custom11,new_custom12,new_custom13,
 new_custom14,new_custom15,new_custom16,new_custom17,new_custom18,new_custom19,new_custom20,
 new_custom21,new_custom22,new_custom23,new_custom24,new_custom25,new_custom26,new_custom27,
@@ -49,7 +49,7 @@ new_custom98,new_custom99,new_custom100
 
 
 const DOCTYPE_ICONS = `
-ai,attachment,audio,csv,eps,excel,exe,flash,gdoc,gdocs,gpres,gsheet,html,image,keynote,
+ai,attachment,audio,box_notes,csv,eps,excel,exe,flash,gdoc,gdocs,gpres,gsheet,html,image,keynote,
 link,mp4,overlay,pack,pages,pdf,ppt,psd,rtf,slide,stypi,txt,unknown,video,visio,
 webex,word,xml,zip
 `
@@ -57,24 +57,24 @@ webex,word,xml,zip
 
 
 const UTILITY_ICONS = `
-add,adduser,announcement,apps,arrowdown,arrowup,attach,back,ban,bold,bookmark,brush,
+add,adduser,announcement,answer,apps,arrowdown,arrowup,attach,back,ban,bold,bookmark,brush,
 bucket,builder,call,capslock,cases,center_align_text,chart,chat,check,checkin,chevrondown,
 chevronleft,chevronright,chevronup,clear,clock,close,comments,company,connected_apps,
-contract_alt,contract,copy,crossfilter,custom_apps,cut,dash,dayview,delete,deprecate,
-desktop,down,download,edit,email,end_call,erect_window,error,event,expand_alt,expand,
-favorite,filter,filterList,forward,frozen,groups,help,home,identity,image,inbox,info,
-insert_tag_field,insert_template,italic,justify_text,kanban,knowledge_base,layout,
-left_align_text,left,like,link,list,location,lock,logout,magicwand,matrix,minimize_window,
-monthlyview,move,muted,new_window,new,news,notebook,notification,office365,offline,
-open_folder,open,opened_folder,package_org_beta,package_org,package,page,palette,paste,
-people,phone_landscape,phone_portrait,photo,power,preview,priority,process,push,puzzle,
+contract,contract_alt,copy,crossfilter,custom_apps,cut,dash,datadotcom,dayview,delete,deprecate,
+description,desktop,down,download,edit,edit_form,email,end_call,erect_window,error,event,expand,
+expand_alt,favorite,feed,file,filter,filterList,forward,frozen,groups,help,home,identity,image,inbox,info,
+insert_tag_field,insert_template,italic,justify_text,kanban,knowledge_base,layers,layout,
+left,left_align_text,like,link,list,location,lock,log_a_call,logout,magicwand,matrix,metrics,minimize_window,
+moneybag,monthlyview,move,muted,new,new_window,news,note,notebook,notification,office365,offline,
+open,open_folder,opened_folder,overflow,package,package_org,package_org_beta,page,palette,paste,
+people,phone_landscape,phone_portrait,photo,picklist,power,preview,priority,process,push,puzzle,
 question,questions_and_answers,record,redo,refresh,relate,remove_formatting,remove_link,
 replace,reply,reset_password,retweet,richtextbulletedlist,richtextindent,richtextnumberedlist,
-richtextoutdent,right_align_text,right,rotate,rows,salesforce1,search,settings,setup_assistant_guide,
-setup,share,shield,side_list,signpost,sms,snippet,socialshare,sort,spinner,standard_objects,
+richtextoutdent,right,right_align_text,rotate,rows,salesforce1,search,settings,setup,setup_assistant_guide,
+share,share_post,shield,side_list,signpost,sms,snippet,socialshare,sort,spinner,standard_objects,
 stop,strikethrough,success,summary,summarydetail,switch,table,tablet_landscape,tablet_portrait,
-text_background_color,text_color,threedots,tile_card_list,topic,trail,undelete,undeprecate,
-underline,undo,unlock,unmuted,up,upload,user,volume_high,volume_low,volume_off,warning,
+tabset,task,text_background_color,text_color,threedots,tile_card_list,topic,touch_action,trail,undelete,undeprecate,
+underline,undo,unlock,unmuted,up,upload,user,user_role,volume_high,volume_low,volume_off,warning,
 weeklyview,world,zoomin,zoomout
 `
 .replace(/^\s+|\s+$/g, '').split(/[\s,]+/);
