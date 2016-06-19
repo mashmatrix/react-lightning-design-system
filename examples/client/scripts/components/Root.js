2e198f9a17e545e5b8dada6ff2b4cf08516eb18f
nudelx <alex.nudelman@samanage.com>
2016-06-19 17:50:27 +0300

    done with export custom icon

--- examples/client/scripts/components/Root.js
+++ examples/client/scripts/components/Root.js
@@ -98,7 +98,7 @@ export default class Root extends React.Component {
                   const Example = SECTIONS[name].klass;
                   const src = SECTIONS[name].src;
                   return (
-                    <div>
+                    <div key={index}>
                       <div style={ { textAlign: 'right' } }>
                         <a target='_blank' href={ `${GITHUB_EXAMPLE_SRC_DIR_URL}${src}.js` }>
                           View source file in Github
