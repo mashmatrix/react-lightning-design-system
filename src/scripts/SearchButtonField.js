23c4368a12cf555f92b44df6a64da9214e32bff6
Eyal Eizenberg <eyal.eizenberg+github@samanage.com>
2016-07-29 14:12:13 +0300

    making the animation even smoother

--- src/scripts/SearchButtonField.js
+++ src/scripts/SearchButtonField.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import Button from './Button';
 import Input from './Input';
+import Icon from './Icon';
 import { registerStyle } from './util';
 import classnames from 'classnames';
 
@@ -9,21 +10,28 @@ export default class SearchButtonField extends React.Component {
     super(props);
     this.state = {
       expanded: false,
+      collapsing: false,
     };
     this.expandField = this.expandField.bind(this);
+    this.collapseField = this.collapseField.bind(this);
     registerStyle('search-button-field', [
       [
         '.search-button-field-container',
         '{ position: relative; display: flex; justify-content: flex-end; }',
       ],
       [
-        '.search-button-field-input.expanded',
-        '{ max-width: 100%; }',
+        '.search-button-field-input',
+        '{ position: absolute; top:0; right: 0; opacity: 0; ' +
+        'max-width: 0; transition: max-width 500ms ease-in-out 300ms,' +
+        ' opacity 300ms ease-in-out 300ms; }',
       ],
       [
-        '.search-button-field-input',
-        '{ position: absolute; top:0; right: 0;' +
-        'max-width: 0; transition: max-width 500ms ease-in-out 100ms; }',
+        '.search-button-field-input.expanded',
+        '{ max-width: 100%; opacity: 1; }',
+      ],
+      [
+        '.search-button-field-input.expanded.collapsing',
+        '{ max-width: 0; }',
       ],
       [
         '.search-button-field-btn',
@@ -31,13 +39,43 @@ export default class SearchButtonField extends React.Component {
       ],
       [
         '.search-button-field-btn.expanded',
-        '{ background-color: white; opacity: 0; }',
+        '{ background-color: white; opacity: 0; cursor: default; }',
+      ],
+      [
+        '.search-button-field-cancel',
+        '{ background-color: #4F6A92; border-radius: 50%;' +
+        'width: 13px; height: 13px; padding: 2px;' +
+        'position: absolute; top: 11px; right: -14px; z-index: 1;' +
+        'opacity: 0; transition: opacity 300ms ease-in-out 300ms; }',
+      ],
+      [
+        '.search-button-field-cancel.expanded',
+        '{ opacity: 1; right: 8px; }',
+      ],
+      [
+        '.search-button-field-cancel.expanded:hover',
+        '{ background-color: #1589ee; cursor: pointer; }',
+      ],
+      [
+        '.search-button-field-cancel.expanded:hover:active',
+        '{ background-color: #0070d2; cursor: pointer; }',
+      ],
+      [
+        '.search-button-field-cancel.expanded.collapsing',
+        '{ opacity: 0; transition: opacity 300ms ease-in-out; }',
       ],
     ]);
   }
 
   expandField() {
-    this.setState({ expanded: !this.state.expanded });
+    this.setState({ expanded: true });
+  }
+
+  collapseField() {
+    this.setState({ collapsing: true });
+    setTimeout(() => {
+      this.setState({ collapsing: false, expanded: false });
+    }, 500);
   }
 
   render() {
@@ -46,14 +84,35 @@ export default class SearchButtonField extends React.Component {
         <Input
           placeholder={this.props.placeholder}
           className={
-            classnames('search-button-field-input', this.state.expanded ? 'expanded' : '')
+            classnames(
+              'search-button-field-input',
+              this.state.expanded ? 'expanded' : '',
+              this.state.collapsing ? 'collapsing' : ''
+            )
           }
+        />
+        <Icon
+          category='action'
+          icon='reject'
+          size='x-small'
+          className={
+            classnames(
+              'search-button-field-cancel',
+              this.state.expanded ? 'expanded' : '',
+              this.state.collapsing ? 'collapsing' : ''
+            )
+          }
+          onClick={this.collapseField}
         />
         <Button
           type='icon-border'
           icon='search'
           className={
-            classnames('search-button-field-btn', this.state.expanded ? 'expanded' : '')
+            classnames(
+              'search-button-field-btn',
+              this.state.expanded ? 'expanded' : '',
+              this.state.collapsing ? 'collapsing' : ''
+            )
           }
           onClick={this.expandField}
         />
