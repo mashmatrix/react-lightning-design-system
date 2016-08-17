9fee529a8cc4c130cb97b4c9179842cc68f98b4c
Alex Karlyuchenko <akarlyuchenko@modeln.com>
2016-08-17 13:22:35 +0300

    Fix dateinput, datepicker component
    main changes:
    Give an ability to specify via props minimum and maximum dates that can be entered(for disable ability to select yesterday via datepicker)

--- examples/client/scripts/components/DatepickerExamples.js
+++ examples/client/scripts/components/DatepickerExamples.js
@@ -25,6 +25,37 @@ export default class DatepickerExamples extends Component {
             />
           </div>
         </div>
+        <h2 className='slds-m-vertical--medium'>Datepicker min max date</h2>
+        <div style={ styles }>
+          <div style={ { width: '20rem' } }>
+            <Datepicker
+              minDate={moment('2016-07-05', 'YYYY-MM-DD').format('YYYY-MM-DD')}
+              maxDate={moment('2016-07-25', 'YYYY-MM-DD').format('YYYY-MM-DD')}
+              selectedDate={ this.state.selectedDate }
+              onSelect={ this.onSelectDate.bind(this) }
+            />
+          </div>
+        </div>
+        <h2 className='slds-m-vertical--medium'>Datepicker min date</h2>
+        <div style={ styles }>
+          <div style={ { width: '20rem' } }>
+            <Datepicker
+              minDate={moment('2016-07-10', 'YYYY-MM-DD').format('YYYY-MM-DD')}
+              selectedDate={ this.state.selectedDate }
+              onSelect={ this.onSelectDate.bind(this) }
+            />
+          </div>
+        </div>
+        <h2 className='slds-m-vertical--medium'>Datepicker max date</h2>
+        <div style={ styles }>
+          <div style={ { width: '20rem' } }>
+            <Datepicker
+              maxDate={moment('2016-07-28', 'YYYY-MM-DD').format('YYYY-MM-DD')}
+              selectedDate={ this.state.selectedDate }
+              onSelect={ this.onSelectDate.bind(this) }
+            />
+          </div>
+        </div>
       </div>
     );
   }
