cd4901811d758c117067bd1888954cface53e8ca
Shinichi Tomita <shinichi.tomita@gmail.com>
2016-12-28 13:53:09 +0900

    add comments for using external svg and svg4everybody polyfill

--- .storybook/config.js
+++ .storybook/config.js
@@ -7,7 +7,10 @@ import { setAssetRoot } from '../src/scripts/util';
 setAddon(infoAddon);
 
 if (/\.sbook\.io/.test(location.hostname)) {
+  // As storybook hub cannot host the static files, use the externally hosted SLDS assets (CORS enabled)
   setAssetRoot('//mashmatrix.github.io/react-lightning-design-system/assets');
+  // As SVG doesn't allow the use of cross-domain external resources (even CORS header is set in resource server)
+  // forcedly use svg4everybody polyfill
   svg4everybody({ polyfill: true });
 }
 
