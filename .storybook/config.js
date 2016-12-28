86a2f6bc948d3fc9f371067862ce2d02619c0941
Shinichi Tomita <shinichi.tomita@gmail.com>
2016-12-28 13:02:21 +0900

    change asset root for storybook hub environment

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,13 @@
 import 'babel-polyfill';
 import { configure, setAddon } from '@kadira/storybook';
 import infoAddon from '@kadira/react-storybook-addon-info';
+import { setAssetRoot } from '../src/scripts/util';
 
 setAddon(infoAddon);
+
+if (/\.sbook\.io/.test(location.hostname)) {
+  setAssetRoot('https://unpkg.com/@salesforce-ux/design-system@2.1.4/assets');
+}
 
 configure(() => {
   require('../stories/index.js');
