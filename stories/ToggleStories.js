7a09dc3a93a9a15ca76d91a31f5eb9a362445fc0
Shinichi Tomita <shinichi.tomita@gmail.com>
2017-02-07 15:42:54 +0900

    add toggle control story

---
+++ stories/ToggleStories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+import { withKnobs, text, boolean } from '@kadira/storybook-addon-knobs';
+import Toggle from '../src/scripts/Toggle';
+
+storiesOf('Toggle', module)
+  .addDecorator(withKnobs)
+  .addWithInfo('Controlled with knobs', 'Toggle controlled with knobs', () => (
+    <Toggle
+      label={ text('label', 'Toggle Label') }
+      required={ boolean('required') }
+      error={ text('error') }
+      checked={ boolean('checked') }
+      disabled={ boolean('disabled') }
+      onChange={ action('change') }
+    />
+  ))
+  .addWithInfo('Default', 'Toggle control', () => (
+    <Toggle onChange={ action('change') } />
+  ))
+  .addWithInfo('Checked', 'Toggle control with checked status', () => (
+    <Toggle checked onChange={ action('change') } />
+  ))
+  .addWithInfo('Disabled', 'Toggle control with disabled status', () => (
+    <Toggle disabled onChange={ action('change') } />
+  ))
+;
--- stories/index.js
+++ stories/index.js
@@ -14,4 +14,5 @@ import './FormStories';
 import './ModalStories';
 import './IconStories';
 import './TabsStories';
+import './ToggleStories';
 import './TreeStories';
