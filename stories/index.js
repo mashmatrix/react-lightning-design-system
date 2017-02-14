a070428c4a3583f735a5dc8ceed6a4c75bed899c
Shinichi Tomita <shinichi.tomita@gmail.com>
2017-02-14 20:26:50 +0900

    add table story

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import './FormStories';
 import './ModalStories';
 import './IconStories';
 import './TabsStories';
+import './TableStories';
 import './ToggleStories';
 import './TreeStories';
 import './PopoverStories';
