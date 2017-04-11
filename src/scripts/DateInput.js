5ffd815ceb728ef37220d75da647d721a62eca9d
Shinichi Tomita <shinichi.tomita@gmail.com>
2017-04-11 09:47:01 +0900

    overwrite the error style of select when rendered in datepicker

--- src/scripts/DateInput.js
+++ src/scripts/DateInput.js
@@ -5,7 +5,7 @@ import FormElement from './FormElement';
 import Input from './Input';
 import Icon from './Icon';
 import Datepicker from './Datepicker';
-import { uuid, isElInChildren } from './util';
+import { uuid, isElInChildren, registerStyle } from './util';
 
 export default class DateInput extends Component {
   constructor(props) {
@@ -23,6 +23,13 @@ export default class DateInput extends Component {
     this.onDatepickerSelect = this.onDatepickerSelect.bind(this);
     this.onDatepickerBlur = this.onDatepickerBlur.bind(this);
     this.onDatepickerClose = this.onDatepickerClose.bind(this);
+
+    registerStyle('dateinput', [
+      [
+        '.slds-has-error .slds-datepicker .slds-select',
+        '{ border: 1px solid #d8dde6; box-shadow: none; }',
+      ],
+    ]);
   }
 
   componentDidUpdate(prevProps, prevState) {
