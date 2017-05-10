a11869a1abd3fad0dbb5bdd1e38f83bc04de6438
Norbert de Langen <ndelangen@me.com>
2017-05-10 22:22:46 +0200

    UPDATE packages by adopting prop-types over react.PropTypes

--- src/scripts/Lookup.js
+++ src/scripts/Lookup.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import FormElement from './FormElement';
 import Input from './Input';
