20863359597b53cbd8d181771b195ff5fde45601
nudelx <alex.nudelman@samanage.com>
2017-11-14 16:18:37 +0200

    f

--- src/scripts/Picklist.js
+++ src/scripts/Picklist.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from './propTypesImport';
 import ReactDOM from 'react-dom';
 import classnames from 'classnames';
 import uuid from 'uuid';
