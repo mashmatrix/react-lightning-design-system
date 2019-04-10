d71234ec74d4048565771da07279dcc6f756f336
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-04-10 15:48:43 +0900

    #289 Badge: Define props as `type` instead of `interface`

--- src/scripts/Badge.tsx
+++ src/scripts/Badge.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import classnames from 'classnames';
 
-export interface BadgeProps {
+export type BadgeProps = {
   type?: 'default' | 'shade' | 'inverse';
   label?: string;
   children: React.ReactNode;
-}
+};
 
 export const Badge = ({ children, type, label, ...props }: BadgeProps) => {
   const typeClassName = type ? `slds-theme--${type}` : null;
