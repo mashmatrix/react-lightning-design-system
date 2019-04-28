348f4e47e3f0bd57c36d155df351b3abde07006e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-04-28 16:50:08 +0900

    #289 Badge: Install react-docgen-typescript-loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,11 @@
+const path = require('path');
+
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
-      {
-        loader: require.resolve('babel-loader'),
-      },
+      require.resolve('babel-loader'),
+      require.resolve('react-docgen-typescript-loader'),
     ],
   });
   config.resolve.extensions.push('.ts', '.tsx');
