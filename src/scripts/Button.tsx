5cf125fcc296c1bc53c1e9afcbe1e15d01553fe7
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-05-14 00:28:29 +0900

    #289 Button: Delete PropTypes

--- src/scripts/Button.tsx
+++ src/scripts/Button.tsx
@@ -1,5 +1,4 @@
 import React, { Component, ReactNode } from 'react';
-import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Icon from './Icon';
 import Spinner from './Spinner';
