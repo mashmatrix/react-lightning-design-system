084342a7eab526c5897a7e3f6678c1a6304eef5c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-06-17 12:19:03 +0900

    #289 Tree: Fix type error of knobs

--- stories/TreeStories.tsx
+++ stories/TreeStories.tsx
@@ -12,57 +12,57 @@ storiesOf('Tree', module).add(
       onNodeClick={action('nodeClick')}
       onNodeToggle={action('nodeToggle')}
       onNodeLabelClick={action('nodeLabelClick')}
-      toggleOnNodeClick={boolean('toggleOnNodeClick')}
+      toggleOnNodeClick={boolean('toggleOnNodeClick', false)}
     >
       <TreeNode
         label={text('label item #1', 'Item #1')}
         opened={boolean('opened item #1', true)}
         selected={boolean('selected item #1', true)}
-        loading={boolean('loading item #1')}
-        leaf={boolean('leaf item #1')}
+        loading={boolean('loading item #1', false)}
+        leaf={boolean('leaf item #1', false)}
       >
         <TreeNode
           label={text('label item #1-1', 'Item #1-1')}
-          opened={boolean('opened item #1-1')}
-          selected={boolean('selected item #1-1')}
-          loading={boolean('loading item #1-1')}
+          opened={boolean('opened item #1-1', false)}
+          selected={boolean('selected item #1-1', false)}
+          loading={boolean('loading item #1-1', false)}
           leaf={boolean('leaf item #1-1', true)}
         />
         <TreeNode
           label={text('label item #1-2', 'Item #1-2')}
-          opened={boolean('opened item #1-2')}
-          selected={boolean('selected item #1-2')}
-          loading={boolean('loading item #1-2')}
-          leaf={boolean('leaf item #1-2')}
+          opened={boolean('opened item #1-2', false)}
+          selected={boolean('selected item #1-2', false)}
+          loading={boolean('loading item #1-2', false)}
+          leaf={boolean('leaf item #1-2', false)}
         >
           <TreeNode
             label={text('label item #1-2-1', 'Item #1-2-1')}
-            opened={boolean('opened item #1-2-1')}
-            selected={boolean('selected item #1-2-1')}
-            loading={boolean('loading item #1-2-1')}
+            opened={boolean('opened item #1-2-1', false)}
+            selected={boolean('selected item #1-2-1', false)}
+            loading={boolean('loading item #1-2-1', false)}
             leaf={boolean('leaf item #1-2-1', true)}
           />
           <TreeNode
             label={text('label item #1-2-2', 'Item #1-2-2')}
-            opened={boolean('opened item #1-2-2')}
-            selected={boolean('selected item #1-2-2')}
-            loading={boolean('loading item #1-2-2')}
+            opened={boolean('opened item #1-2-2', false)}
+            selected={boolean('selected item #1-2-2', false)}
+            loading={boolean('loading item #1-2-2', false)}
             leaf={boolean('leaf item #1-2-2', true)}
           />
         </TreeNode>
         <TreeNode
           label={text('label item #1-3', 'Item #1-3')}
-          opened={boolean('opened item #1-3')}
-          selected={boolean('selected item #1-3')}
-          loading={boolean('loading item #1-3')}
+          opened={boolean('opened item #1-3', false)}
+          selected={boolean('selected item #1-3', false)}
+          loading={boolean('loading item #1-3', false)}
           leaf={boolean('leaf item #1-3', true)}
         />
       </TreeNode>
       <TreeNode
         label={text('label item #2', 'Item #2')}
-        opened={boolean('opened item #2')}
-        selected={boolean('selected item #2')}
-        loading={boolean('loading item #2')}
+        opened={boolean('opened item #2', false)}
+        selected={boolean('selected item #2', false)}
+        loading={boolean('loading item #2', false)}
         leaf={boolean('leaf item #2', true)}
       />
     </Tree>
