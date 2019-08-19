0d426ae818b8cc267b9c21b01b97f45c9bfb987d
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-08-20 00:49:37 +0900

    #289 FieldSet: Convert PropTypes to TypeScript types

--- src/scripts/FieldSet.tsx
+++ src/scripts/FieldSet.tsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { FormElement } from './FormElement';
 import { uuid } from './util';
 
-const FieldSet = ({ className, label, children, ...props }) => {
+export type FieldSetProps = {
+  className?: string;
+  label?: string;
+};
+
+const FieldSet: React.FC<FieldSetProps> = ({
+  className,
+  label,
+  children,
+  ...props
+}) => {
   const fsClassNames = classnames(className, 'slds-form--compound');
   return (
     <fieldset className={fsClassNames} {...props}>
@@ -16,16 +25,17 @@ const FieldSet = ({ className, label, children, ...props }) => {
   );
 };
 
-FieldSet.propTypes = {
-  className: PropTypes.string,
-  label: PropTypes.string,
-  children: PropTypes.node,
-};
-
 FieldSet.isFormElement = true;
 
-class Row extends Component {
-  renderChild(totalCols, child) {
+type RowProps = {
+  className?: string;
+  cols?: number;
+};
+
+class Row extends Component<RowProps> {
+  static isFormElement = true;
+
+  renderChild(totalCols: number, child: any) {
     if (child && !child.type.isFormElement) {
       const { id = `form-element-${uuid()}` } = child.props;
       const formElemProps = { id, totalCols, cols: 1 };
@@ -49,14 +59,6 @@ class Row extends Component {
     );
   }
 }
-
-Row.propTypes = {
-  className: PropTypes.string,
-  cols: PropTypes.number,
-  children: PropTypes.node,
-};
-
-Row.isFormElement = true;
 
 FieldSet.Row = Row;
 
