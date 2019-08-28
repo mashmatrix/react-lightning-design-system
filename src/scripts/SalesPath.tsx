141906aacc978a79006b02d153f228186042baf8
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-08-29 00:46:50 +0900

    #289 SalesPath: Convert PropTypes to TypeScript types

--- src/scripts/SalesPath.tsx
+++ src/scripts/SalesPath.tsx
@@ -1,77 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Icon } from './Icon';
 
-class SalesPath extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-
-    this.onItemClick = this.onItemClick.bind(this);
-  }
-
-  onItemClick(itemKey) {
-    if (this.props.onSelect) {
-      this.props.onSelect(itemKey);
-    }
-    // Uncontrolled
-    this.setState({ activeKey: itemKey });
-  }
-
-  renderSalesPath(activeKey, paths) {
-    let typeTracker = -1;
-
-    return React.Children.map(paths, (path) => {
-      const { eventKey, type, ...props } = path.props;
-      const isActive = eventKey === activeKey;
-
-      typeTracker = isActive ? 0 : typeTracker >= 0 ? 1 : -1;
-
-      const evaluatedType =
-        type ||
-        (isActive ? 'current' : typeTracker === -1 ? 'complete' : 'incomplete');
-
-      return (
-        <PathItem
-          eventKey={eventKey}
-          type={evaluatedType}
-          onSelect={this.onItemClick}
-          {...props}
-        />
-      );
-    });
-  }
-
-  render() {
-    const { className, children } = this.props;
-    const activeKey =
-      this.props.activeKey ||
-      this.state.activeKey ||
-      this.props.defaultActiveKey;
-
-    const salesPathClassNames = classnames(className, 'slds-tabs--path');
-    return (
-      <div className={salesPathClassNames} role='application tablist'>
-        <ul className='slds-tabs--path__nav' role='presentation'>
-          {this.renderSalesPath(activeKey, children)}
-        </ul>
-      </div>
-    );
-  }
-}
-
-SalesPath.propTypes = {
-  className: PropTypes.string,
-  onSelect: PropTypes.func,
-  children: PropTypes.node,
-  /* eslint-disable react/forbid-prop-types */
-  defaultActiveKey: PropTypes.any,
-  activeKey: PropTypes.any,
+export type PathItemProps = {
+  className?: string;
+  eventKey?: any;
+  type?: 'complete' | 'current' | 'incomplete';
+  title?: string;
+  completedTitle?: string;
+  onSelect?: (itemKey: any) => void;
 };
 
-class PathItem extends React.Component {
-  onItemClick(itemKey) {
+class PathItem extends React.Component<PathItemProps> {
+  onItemClick(itemKey: any) {
     if (this.props.onSelect) {
       this.props.onSelect(itemKey);
     }
@@ -112,15 +53,75 @@ class PathItem extends React.Component {
   }
 }
 
-PathItem.propTypes = {
-  className: PropTypes.string,
-  eventKey: PropTypes.any,
-  type: PropTypes.oneOf(['complete', 'current', 'incomplete']),
-  title: PropTypes.string,
-  completedTitle: PropTypes.string,
-  onSelect: PropTypes.func,
+export type SalesPathProps = {
+  className?: string;
+  defaultActiveKey?: any;
+  activeKey?: any;
+  onSelect?: (itemKey: any) => void;
 };
 
-SalesPath.PathItem = PathItem;
+export type SalesPathState = {
+  activeKey?: any;
+};
+
+class SalesPath extends React.Component<SalesPathProps, SalesPathState> {
+  static PathItem = PathItem;
+
+  constructor(props: Readonly<SalesPathProps>) {
+    super(props);
+    this.state = {};
+
+    this.onItemClick = this.onItemClick.bind(this);
+  }
+
+  onItemClick(itemKey: any) {
+    if (this.props.onSelect) {
+      this.props.onSelect(itemKey);
+    }
+    // Uncontrolled
+    this.setState({ activeKey: itemKey });
+  }
+
+  renderSalesPath(activeKey: any, paths: any) {
+    let typeTracker = -1;
+
+    return React.Children.map(paths, (path) => {
+      const { eventKey, type, ...props } = path.props;
+      const isActive = eventKey === activeKey;
+
+      typeTracker = isActive ? 0 : typeTracker >= 0 ? 1 : -1;
+
+      const evaluatedType =
+        type ||
+        (isActive ? 'current' : typeTracker === -1 ? 'complete' : 'incomplete');
+
+      return (
+        <PathItem
+          eventKey={eventKey}
+          type={evaluatedType}
+          onSelect={this.onItemClick}
+          {...props}
+        />
+      );
+    });
+  }
+
+  render() {
+    const { className, children } = this.props;
+    const activeKey =
+      this.props.activeKey ||
+      this.state.activeKey ||
+      this.props.defaultActiveKey;
+
+    const salesPathClassNames = classnames(className, 'slds-tabs--path');
+    return (
+      <div className={salesPathClassNames} role='application tablist'>
+        <ul className='slds-tabs--path__nav' role='presentation'>
+          {this.renderSalesPath(activeKey, children)}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default SalesPath;
