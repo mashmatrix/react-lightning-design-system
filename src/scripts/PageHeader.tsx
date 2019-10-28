aee2a60b9b0c9dff46015e02cf67f9f2eb2fcf0c
Shinichi Tomita <shinichi.tomita@gmail.com>
2019-10-28 19:14:21 +0900

    fix lint error

--- src/scripts/PageHeader.tsx
+++ src/scripts/PageHeader.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactElement } from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 
 import { MediaObject } from './MediaObject';
