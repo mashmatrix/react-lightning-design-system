7dfa0f34e587c389bef0facb986ac0a9029a95eb
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-01-09 02:28:46 +0900

    Add PillStories

---
+++ stories/PillStories.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { text, boolean } from '@storybook/addon-knobs';
+import { Pill } from '../src/scripts';
+
+storiesOf('Pill', module)
+  .add(
+    'Controlled with knobs',
+    () => (
+      <Pill
+        label={text('label', 'Pill Label')}
+        truncate={boolean('truncate', false)}
+        disabled={boolean('disabled', false)}
+        onClick={action('onclick')}
+        onRemove={action('onRemove')}
+      />
+    ),
+    { info: 'Pill controlled with knobs' },
+  )
+  .add(
+    'with icon',
+    () => (
+      <Pill
+        label='Pill Label'
+        icon={{ category: 'standard', icon: 'account' }}
+      />
+    ),
+  {
+    info: 'Pill with icon',
+  },
+  )
+  .add('disabled', () => <Pill label='Pill Label' disabled />, {
+    info: 'Pill with disabled status',
+  })
+  .add(
+    'truncate',
+    () => (
+      <div style={{ width: '200px' }}>
+        <div className='slds-pill__container'>
+          <Pill
+            label='Pill Label that is longer than the area that contains it'
+            truncate
+          />
+        </div>
+      </div>
+    ),
+  {
+    info: 'Pill with truncated label',
+  },
+  );
