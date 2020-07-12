a3493e63e02c2c92e44d6aa79b56d22299a3c591
Shinichi Tomita <shinichi.tomita@gmail.com>
2020-07-12 17:48:13 +0900

    generate module output for tree shaking

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/env',
+      process.env.BUILD_TARGET === 'module'
+        ? {
+            modules: false,
+          }
+        : {},
+    ],
+    '@babel/react',
+    '@babel/typescript',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-class-properties',
+  ],
+  sourceMaps: 'inline',
+};
