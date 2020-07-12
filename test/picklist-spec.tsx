9815872056145620d88cc959fc71c466cd29986d
Shinichi Tomita <shinichi.tomita@gmail.com>
2020-07-13 00:00:22 +0900

    let Picklist to use onSelect handler to accept selected value

--- test/picklist-spec.tsx
+++ test/picklist-spec.tsx
@@ -40,7 +40,7 @@ describe('Picklist', () => {
   });
 
   it('click should trigger onValueChange with a single value', () => {
-    const store = { value: 2 };
+    const store = { value: 2 as any };
     const wrapper = mount(
       <Picklist
         label='Pick List'
@@ -50,16 +50,20 @@ describe('Picklist', () => {
           store.value = v;
         }}
       >
-        <PicklistItem value={1}>Item #1</PicklistItem>
-        <PicklistItem value={2}>Item #2</PicklistItem>
+        <PicklistItem value={1} className='value1'>
+          Item #1
+        </PicklistItem>
+        <PicklistItem value={2} className='value2'>
+          Item #2
+        </PicklistItem>
       </Picklist>
     );
-    wrapper.find('.react-slds-menuitem[value=1]').simulate('click');
+    wrapper.find('.slds-dropdown__item.value1 a').simulate('click');
     expect(store.value).toEqual(1);
   });
 
   it('click on multiselect should trigger onValueChange with a array value', () => {
-    const store = { value: 2 };
+    const store = { value: 2 as any };
     const wrapper = mount(
       <Picklist
         label='Pick List'
@@ -70,11 +74,15 @@ describe('Picklist', () => {
           store.value = v;
         }}
       >
-        <PicklistItem value={1}>Item #1</PicklistItem>
-        <PicklistItem value={2}>Item #2</PicklistItem>
+        <PicklistItem value={1} className='value1'>
+          Item #1
+        </PicklistItem>
+        <PicklistItem value={2} className='value2'>
+          Item #2
+        </PicklistItem>
       </Picklist>
     );
-    wrapper.find('.react-slds-menuitem[value=1]').simulate('click');
+    wrapper.find('.slds-dropdown__item.value1 a').simulate('click');
     expect(store.value).toEqual([2, 1]);
   });
 });
