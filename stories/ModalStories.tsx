de91c173ee337b2d539b7c0bc52046dbbdf2bf97
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-11-25 01:39:21 +0900

    Modal: add stories

--- stories/ModalStories.tsx
+++ stories/ModalStories.tsx
@@ -161,6 +161,82 @@ storiesOf('Modal', module)
     }
   )
   .add(
+    'With tagline',
+    () => (
+      <Modal opened onHide={action('hide')}>
+        <Header
+          title='Modal with tagline'
+          tagline='This is a tagline'
+          closeButton
+        />
+        <Content className='slds-p-around--small'>
+          <p>
+            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud
+            ullamco deserunt aute id consequat veniam incididunt duis in sint
+            irure nisi. Mollit officia cillum Lorem ullamco minim nostrud elit
+            officia tempor esse quis. Cillum sunt ad dolore quis aute consequat
+            ipsum magna exercitation reprehenderit magna. Tempor cupidatat
+            consequat elit dolor adipisicing.
+          </p>
+          <p>
+            Dolor eiusmod sunt ex incididunt cillum quis nostrud velit duis sit
+            officia. Lorem aliqua enim laboris do dolor eiusmod officia. Mollit
+            incididunt nisi consectetur esse laborum eiusmod pariatur proident.
+            Eiusmod et adipisicing culpa deserunt nostrud ad veniam nulla aute
+            est. Labore esse esse cupidatat amet velit id elit consequat minim
+            ullamco mollit enim excepteur ea.
+          </p>
+        </Content>
+        <Footer>
+          <Button type='neutral' label='Cancel' />
+          <Button type='brand' label='Done' />
+        </Footer>
+      </Modal>
+    ),
+    {
+      info: {
+        text: 'Modal dialog with tagline',
+        inline: false,
+      },
+    }
+  )
+  .add(
+    'Footer directional',
+    () => (
+      <Modal opened onHide={action('hide')}>
+        <Header title='Modal with directional footer' closeButton />
+        <Content className='slds-p-around--small'>
+          <p>
+            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud
+            ullamco deserunt aute id consequat veniam incididunt duis in sint
+            irure nisi. Mollit officia cillum Lorem ullamco minim nostrud elit
+            officia tempor esse quis. Cillum sunt ad dolore quis aute consequat
+            ipsum magna exercitation reprehenderit magna. Tempor cupidatat
+            consequat elit dolor adipisicing.
+          </p>
+          <p>
+            Dolor eiusmod sunt ex incididunt cillum quis nostrud velit duis sit
+            officia. Lorem aliqua enim laboris do dolor eiusmod officia. Mollit
+            incididunt nisi consectetur esse laborum eiusmod pariatur proident.
+            Eiusmod et adipisicing culpa deserunt nostrud ad veniam nulla aute
+            est. Labore esse esse cupidatat amet velit id elit consequat minim
+            ullamco mollit enim excepteur ea.
+          </p>
+        </Content>
+        <Footer directional>
+          <Button type='neutral' label='Cancel' />
+          <Button type='brand' label='Done' />
+        </Footer>
+      </Modal>
+    ),
+    {
+      info: {
+        text: 'Modal dialog with directional footer',
+        inline: false,
+      },
+    }
+  )
+  .add(
     'Form elements',
     () => (
       <Modal opened onHide={action('hide')}>
