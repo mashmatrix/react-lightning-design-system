d4bed287eeb7652f8e3407e5394b56b488597df5
Shinichi Tomita <shinichi.tomita@gmail.com>
2022-04-24 22:37:07 +0900

    fix to include preview.js in .storybook

--- .storybook/wrapSLDS.js
+++ .storybook/wrapSLDS.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import classnames from 'classnames';
 import { ComponentSettings } from '../src/scripts';
 
-export default function wrapContent(options) {
+export default function wrapSLDS(options) {
   return story => (
     <ComponentSettings assetRoot={ options.assetRoot } portalClassName='slds'>
-      <div className={ classnames('content-wrapper', options.className) }>{ story() }</div>
+      {story()}
     </ComponentSettings>
   );
 }
