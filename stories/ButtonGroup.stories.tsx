7eafce9e946ab73f7ae151419a7873fbc647c6d3
Shinichi Tomita <shinichi.tomita@gmail.com>
2022-04-26 21:36:00 +0900

    adjust button group story as csf3

--- stories/ButtonGroup.stories.tsx
+++ stories/ButtonGroup.stories.tsx
@@ -1,43 +1,75 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
+import React, { ComponentProps } from 'react';
 import { ButtonGroup, Button, DropdownButton, MenuItem } from '../src/scripts';
+import { Meta, StoryObj, DecoratorFn } from '@storybook/react';
+
+/**
+ *
+ */
 const darkBgStyle = {
   backgroundColor: '#16325c',
   padding: 4,
 };
-export default {
+
+const darkBgDecorator: DecoratorFn = (Story) => (
+  <div style={darkBgStyle}>
+    <Story />
+  </div>
+);
+
+/**
+ *
+ */
+type StoryProps = ComponentProps<typeof ButtonGroup> &
+  Pick<ComponentProps<typeof Button>, 'onClick'> &
+  Pick<ComponentProps<typeof DropdownButton>, 'onMenuSelect'>;
+
+/**
+ *
+ */
+const meta: Meta<StoryProps> = {
   title: 'ButtonGroup',
+  component: ButtonGroup,
+  argTypes: {
+    onClick: { action: 'click' },
+    onMenuSelect: { action: 'menuSelect' },
+  },
 };
-export const Default = {
-  render: () => (
-    <ButtonGroup>
-      <Button type='neutral' onClick={action('click')}>
+export default meta;
+
+/**
+ *
+ */
+export const Default: StoryObj<StoryProps> = {
+  render: ({ onClick, ...args }) => (
+    <ButtonGroup {...args}>
+      <Button type='neutral' onClick={onClick}>
         Refresh
       </Button>
-      <Button type='neutral' onClick={action('click')}>
+      <Button type='neutral' onClick={onClick}>
         Edit
       </Button>
-      <Button
-        type='neutral'
-        icon='download'
-        iconAlign='left'
-        onClick={action('click')}
-      >
+      <Button type='neutral' icon='download' iconAlign='left' onClick={onClick}>
         Download
       </Button>
     </ButtonGroup>
   ),
   parameters: {
-    info: 'Default grouped buttons',
+    docs: {
+      storyDescription: 'Default grouped buttons',
+    },
   },
 };
-export const DefaultDisalbed = {
-  render: () => (
-    <ButtonGroup>
-      <Button type='neutral' onClick={action('click')}>
+
+/**
+ *
+ */
+export const DefaultDisabled: StoryObj<StoryProps> = {
+  render: ({ onClick, ...args }) => (
+    <ButtonGroup {...args}>
+      <Button type='neutral' onClick={onClick}>
         Refresh
       </Button>
-      <Button type='neutral' onClick={action('click')}>
+      <Button type='neutral' onClick={onClick}>
         Edit
       </Button>
       <Button
@@ -45,38 +77,38 @@ export const DefaultDisalbed = {
         disabled
         icon='download'
         iconAlign='left'
-        onClick={action('click')}
+        onClick={onClick}
       >
         Download
       </Button>
     </ButtonGroup>
   ),
-  name: 'Default disalbed',
   parameters: {
-    info: 'Grouped buttons with disabled button',
+    docs: {
+      storyDescription: 'Grouped buttons with disabled button',
+    },
   },
 };
-export const More = {
-  render: () => (
-    <ButtonGroup>
-      <Button type='neutral' onClick={action('click')}>
+
+/**
+ *
+ */
+export const More: StoryObj<StoryProps> = {
+  render: ({ onClick, onMenuSelect, ...args }) => (
+    <ButtonGroup {...args}>
+      <Button type='neutral' onClick={onClick}>
         Refresh
       </Button>
-      <Button type='neutral' onClick={action('click')}>
+      <Button type='neutral' onClick={onClick}>
         Edit
       </Button>
-      <Button
-        type='neutral'
-        icon='download'
-        iconAlign='left'
-        onClick={action('click')}
-      >
+      <Button type='neutral' icon='download' iconAlign='left' onClick={onClick}>
         Download
       </Button>
       <DropdownButton
         type='icon-border'
         menuAlign='right'
-        onMenuSelect={action('menuSelect')}
+        onMenuSelect={onMenuSelect}
       >
         <MenuItem eventKey={1}>Menu Item One</MenuItem>
         <MenuItem eventKey={2}>Menu Item Two</MenuItem>
@@ -85,40 +117,42 @@ export const More = {
     </ButtonGroup>
   ),
   parameters: {
-    info: 'Grouped buttons with dropdown button in right',
+    docs: {
+      storyDescription: 'Grouped buttons with dropdown button in right',
+    },
   },
 };
-export const Inverse = {
-  render: () => (
-    <div style={darkBgStyle}>
-      <ButtonGroup>
-        <Button type='inverse' onClick={action('click')}>
-          Refresh
-        </Button>
-        <Button type='inverse' onClick={action('click')}>
-          Edit
-        </Button>
-        <Button
-          type='inverse'
-          icon='download'
-          iconAlign='left'
-          onClick={action('click')}
-        >
-          Download
-        </Button>
-        <DropdownButton
-          type='icon-border-inverse'
-          menuAlign='right'
-          onMenuSelect={action('menuSelect')}
-        >
-          <MenuItem eventKey={1}>Menu Item One</MenuItem>
-          <MenuItem eventKey={2}>Menu Item Two</MenuItem>
-          <MenuItem eventKey={3}>Menu Item Three</MenuItem>
-        </DropdownButton>
-      </ButtonGroup>
-    </div>
+
+/**
+ *
+ */
+export const Inverse: StoryObj<StoryProps> = {
+  render: ({ onClick, onMenuSelect, ...args }) => (
+    <ButtonGroup {...args}>
+      <Button type='inverse' onClick={onClick}>
+        Refresh
+      </Button>
+      <Button type='inverse' onClick={onClick}>
+        Edit
+      </Button>
+      <Button type='inverse' icon='download' iconAlign='left' onClick={onClick}>
+        Download
+      </Button>
+      <DropdownButton
+        type='icon-border-inverse'
+        menuAlign='right'
+        onMenuSelect={onMenuSelect}
+      >
+        <MenuItem eventKey={1}>Menu Item One</MenuItem>
+        <MenuItem eventKey={2}>Menu Item Two</MenuItem>
+        <MenuItem eventKey={3}>Menu Item Three</MenuItem>
+      </DropdownButton>
+    </ButtonGroup>
   ),
+  decorators: [darkBgDecorator],
   parameters: {
-    info: 'Grouped buttons with inversed color',
+    docs: {
+      storyDescription: 'Grouped buttons with inversed color',
+    },
   },
 };
