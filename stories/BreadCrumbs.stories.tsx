3765a0cf5e00185eb99bc23d5b3f627fa5c00770
Shinichi Tomita <shinichi.tomita@gmail.com>
2022-04-26 10:17:03 +0900

    adjust breadcrumbs story as csf3

--- stories/BreadCrumbs.stories.tsx
+++ stories/BreadCrumbs.stories.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { BreadCrumbs, Crumb } from '../src/scripts';
-export default {
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+
+/**
+ *
+ */
+const meta: ComponentMeta<typeof BreadCrumbs> = {
   title: 'BreadCrumbs',
+  component: BreadCrumbs,
 };
-export const Default = {
-  render: () => (
-    <BreadCrumbs>
-      <Crumb onClick={action('crumb1#click')}>Parent Entity</Crumb>
-      <Crumb onClick={action('crumb2#click')}>Parent Record Name</Crumb>
-    </BreadCrumbs>
-  ),
+export default meta;
+
+/**
+ *
+ */
+export const Default: ComponentStoryObj<typeof BreadCrumbs> = {
+  render(args) {
+    return (
+      <BreadCrumbs {...args}>
+        <Crumb onClick={action('crumb1#click')}>Parent Entity</Crumb>
+        <Crumb onClick={action('crumb2#click')}>Parent Record Name</Crumb>
+      </BreadCrumbs>
+    );
+  },
   parameters: {
-    info: 'Default BreadCrumbs',
+    docs: {
+      storyDescription: 'Default BreadCrumbs',
+    },
   },
 };
