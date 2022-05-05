5aac883c8e24bdc661ebc1f2ac32ba486aea54fc
Shinichi Tomita <shinichi.tomita@gmail.com>
2022-05-05 17:36:32 +0900

    change Badge component to FC

--- src/scripts/Badge.tsx
+++ src/scripts/Badge.tsx
@@ -1,12 +1,18 @@
-import React, { HTMLAttributes } from 'react';
+import React, { FC, HTMLAttributes } from 'react';
 import classnames from 'classnames';
 
+/**
+ *
+ */
 export type BadgeProps = {
   type?: 'default' | 'shade' | 'inverse';
   label?: string;
 } & HTMLAttributes<HTMLSpanElement>;
 
-export const Badge: React.FC<BadgeProps> = ({ type, label, ...props }) => {
+/**
+ *
+ */
+export const Badge: FC<BadgeProps> = ({ type, label, ...props }) => {
   const typeClassName = type ? `slds-theme_${type}` : null;
   const badgeClassNames = classnames('slds-badge', typeClassName);
   return (
