f0998ba53f26543cc08d001dd5788a0e9cfe4e50
Shinichi Tomita <shinichi.tomita@gmail.com>
2022-05-05 16:19:47 +0900

    change ButtonGroup component to FC

--- src/scripts/ButtonGroup.tsx
+++ src/scripts/ButtonGroup.tsx
@@ -1,39 +1,40 @@
-import React, { Component, Children, HTMLAttributes } from 'react';
+import React, { Children, HTMLAttributes, FC, createContext } from 'react';
 import classnames from 'classnames';
-import { DropdownButton } from './DropdownButton';
 
-export type ButtonGroupProps = {} & HTMLAttributes<HTMLDivElement>;
+/**
+ *
+ */
+export type ButtonGroupProps = HTMLAttributes<HTMLDivElement>;
 
-export class ButtonGroup extends Component<ButtonGroupProps, {}> {
-  constructor(props: Readonly<ButtonGroupProps>) {
-    super(props);
-    this.renderButton = this.renderButton.bind(this);
-  }
+/**
+ *
+ */
+export const ButtonGroupContext = createContext<{
+  grouped: true;
+  isFirstInGroup: boolean;
+  isLastInGroup: boolean;
+} | null>(null);
 
-  renderButton(button: any, index: number) {
-    const cnt = React.Children.count(this.props.children);
-    if (
-      button.type &&
-      (button.type === DropdownButton || button.type.isGroupable)
-    ) {
-      return React.cloneElement(button, {
-        key: index,
-        grouped: true,
-        isFirstInGroup: index === 0,
-        isLastInGroup: index === cnt - 1,
-      });
-    }
-
-    return button;
-  }
-
-  render() {
-    const { className, children, ...props } = this.props;
-    const btnGrpClassNames = classnames(className, 'slds-button-group');
-    return (
-      <div className={btnGrpClassNames} role='group' {...props}>
-        {Children.map(children, this.renderButton)}
-      </div>
-    );
-  }
-}
+/**
+ *
+ */
+export const ButtonGroup: FC<ButtonGroupProps> = (props) => {
+  const { className, children, ...rprops } = props;
+  const btnGrpClassNames = classnames(className, 'slds-button-group');
+  const cnt = React.Children.count(children);
+  return (
+    <div className={btnGrpClassNames} role='group' {...rprops}>
+      {Children.map(children, (child, index) => (
+        <ButtonGroupContext.Provider
+          value={{
+            grouped: true,
+            isFirstInGroup: index === 0,
+            isLastInGroup: index === cnt - 1,
+          }}
+        >
+          {child}
+        </ButtonGroupContext.Provider>
+      ))}
+    </div>
+  );
+};
