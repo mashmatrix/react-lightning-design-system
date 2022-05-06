7d3d5573f3ca3c5e290c05f65e0237320ce0dfa0
Shinichi Tomita <shinichi.tomita@gmail.com>
2022-05-06 19:11:20 +0900

    change Textarea component to FC

--- src/scripts/Textarea.tsx
+++ src/scripts/Textarea.tsx
@@ -1,49 +1,40 @@
-import React, { Component, TextareaHTMLAttributes } from 'react';
+import React, { FC, Ref, TextareaHTMLAttributes, useContext } from 'react';
 import classnames from 'classnames';
 import { FormElement, FormElementProps } from './FormElement';
-import { uuid } from './util';
+import { FieldSetRowContext } from './FieldSet';
+import { useFormElementId } from './hooks';
 
+/**
+ *
+ */
 export type TextareaProps = {
   label?: string;
   required?: boolean;
   error?: FormElementProps['error'];
-  totalCols?: number;
   cols?: number;
-  textareaRef?: (...args: any[]) => any;
+  textareaRef?: Ref<HTMLTextAreaElement>;
 } & TextareaHTMLAttributes<HTMLTextAreaElement>;
 
-type TextareaState = {
-  id: string;
-};
-
-export class Textarea extends Component<TextareaProps, TextareaState> {
-  static isFormElement = true;
-
-  constructor(props: Readonly<TextareaProps>) {
-    super(props);
-    this.state = { id: `form-element-${uuid()}` };
-  }
-
-  render() {
-    const id = this.props.id || this.state.id;
-    const { label, required, error, totalCols, cols, ...props } = this.props;
-    if (label || required || error || totalCols || cols) {
-      const formElemProps = { id, label, required, error, totalCols, cols };
-      return (
-        <FormElement {...formElemProps}>
-          <Textarea {...{ ...props, id }} />
-        </FormElement>
-      );
-    }
-    const { className, textareaRef, ...pprops } = props;
-    const taClassNames = classnames(className, 'slds-input');
+/**
+ *
+ */
+export const Textarea: FC<TextareaProps> = (props) => {
+  const { id: id_, label, required, error, cols, ...rprops } = props;
+  const id = useFormElementId(id_, 'textarea');
+  const { totalCols } = useContext(FieldSetRowContext) ?? {};
+  if (label || required || error || totalCols || cols) {
+    const formElemProps = { id, label, required, error, totalCols, cols };
     return (
-      <textarea
-        id={id}
-        ref={textareaRef}
-        className={taClassNames}
-        {...pprops}
-      />
+      <FormElement {...formElemProps}>
+        <Textarea {...{ ...rprops, id }} />
+      </FormElement>
     );
   }
-}
+  const { className, textareaRef, ...rprops2 } = rprops;
+  const taClassNames = classnames(className, 'slds-input');
+  return (
+    <textarea id={id} ref={textareaRef} className={taClassNames} {...rprops2} />
+  );
+};
+
+(Textarea as unknown as { isFormElement?: boolean }).isFormElement = true;
