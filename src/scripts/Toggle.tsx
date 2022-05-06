cdfc057e3913ee438101f15c85a715abc27a7ca5
Shinichi Tomita <shinichi.tomita@gmail.com>
2022-05-06 19:27:10 +0900

    change Toggle component to FC

--- src/scripts/Toggle.tsx
+++ src/scripts/Toggle.tsx
@@ -1,54 +1,52 @@
-import React, { Component, InputHTMLAttributes } from 'react';
+import React, { FC, InputHTMLAttributes, useRef } from 'react';
 import classnames from 'classnames';
 import { FormElement, FormElementProps } from './FormElement';
+import { useFormElementId } from './hooks';
 
+/**
+ *
+ */
 export type ToggleProps = {
   label?: string;
   required?: boolean;
   error?: FormElementProps['error'];
-  totalCols?: number;
   cols?: number;
   name?: string;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-export class Toggle extends Component<ToggleProps> {
-  node: HTMLDivElement | null = null;
+/**
+ *
+ */
+export const Toggle: FC<ToggleProps> = (props) => {
+  const nodeRef = useRef<HTMLDivElement | null>(null);
+  const { id: id_, className, label, required, error, cols, ...rprops } = props;
+  const id = useFormElementId(id_, 'toggle');
+  const toggleClassNames = classnames(
+    className,
+    'slds-checkbox_toggle slds-grid'
+  );
+  const toggle = (
+    <label className={toggleClassNames}>
+      <input
+        id={id}
+        name='checkbox'
+        type='checkbox'
+        aria-describedby='toggle-desc'
+        {...rprops}
+      />
+      <span className='slds-checkbox_faux_container' aria-live='assertive'>
+        <span className='slds-checkbox_faux' />
+        <span className='slds-checkbox_on'>Enabled</span>
+        <span className='slds-checkbox_off'>Disabled</span>
+      </span>
+    </label>
+  );
+  const formElemProps = { id, label, required, error, cols };
+  return (
+    <FormElement formElementRef={nodeRef} {...formElemProps}>
+      {toggle}
+    </FormElement>
+  );
+};
 
-  renderToggle({ className, label, ...props }: ToggleProps) {
-    const toggleClassNames = classnames(
-      className,
-      'slds-checkbox_toggle slds-grid'
-    );
-    return (
-      <label className={toggleClassNames}>
-        <span className='slds-form-element__label slds-m-bottom_none'>
-          {label}
-        </span>
-        <input
-          name='checkbox'
-          type='checkbox'
-          aria-describedby='toggle-desc'
-          {...props}
-        />
-        <span className='slds-checkbox_faux_container' aria-live='assertive'>
-          <span className='slds-checkbox_faux' />
-          <span className='slds-checkbox_on'>Enabled</span>
-          <span className='slds-checkbox_off'>Disabled</span>
-        </span>
-      </label>
-    );
-  }
-
-  render() {
-    const { required, error, totalCols, cols, ...props } = this.props;
-    const formElemProps = { required, error, totalCols, cols };
-    return (
-      <FormElement
-        formElementRef={(node) => (this.node = node)}
-        {...formElemProps}
-      >
-        {this.renderToggle(props)}
-      </FormElement>
-    );
-  }
-}
+(Toggle as unknown as { isFormElement?: boolean }).isFormElement = true;
