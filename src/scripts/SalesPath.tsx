e58435da9e33136b6c177b57243322d8bcce3d63
Shinichi Tomita <shinichi.tomita@gmail.com>
2022-05-07 21:29:37 +0900

    change SalesPath component to FC

--- src/scripts/SalesPath.tsx
+++ src/scripts/SalesPath.tsx
@@ -1,125 +1,158 @@
-import React from 'react';
+import React, {
+  FC,
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+} from 'react';
 import classnames from 'classnames';
 import { Icon } from './Icon';
+import { useControlledValue } from './hooks';
 
-export type PathItemProps = {
+/**
+ *
+ */
+type SalesPathItemType = 'complete' | 'current' | 'incomplete';
+
+type SalesPathKey = string | number;
+
+/**
+ *
+ */
+const SalesPathTypeContext = createContext<SalesPathItemType>('incomplete');
+
+const SalesPathHandlersContext = createContext<{
+  onSelect?: (eventKey: SalesPathKey) => void;
+}>({});
+
+/**
+ *
+ */
+export type SalesPathItemProps = {
   className?: string;
-  eventKey?: any;
-  type?: 'complete' | 'current' | 'incomplete';
+  eventKey?: SalesPathKey;
+  type?: SalesPathItemType;
   title?: string;
   completedTitle?: string;
-  onSelect?: (itemKey: any) => void;
 };
 
-class PathItem extends React.Component<PathItemProps> {
-  onItemClick(itemKey: any) {
-    if (this.props.onSelect) {
-      this.props.onSelect(itemKey);
+/**
+ *
+ */
+export const SalesPathItem: FC<SalesPathItemProps> = (props) => {
+  const { className, eventKey, type: type_, title, completedTitle } = props;
+  const evaluatedType = useContext(SalesPathTypeContext);
+  const { onSelect } = useContext(SalesPathHandlersContext);
+  const type = type_ ?? evaluatedType;
+
+  const onItemClick = useCallback(() => {
+    if (eventKey != null) {
+      onSelect?.(eventKey);
     }
-  }
+  }, [onSelect, eventKey]);
 
-  render() {
-    const { className, eventKey, title, completedTitle, type } = this.props;
+  const pathItemClassName = classnames(
+    'slds-tabs_path__item',
+    type ? `slds-is-${type}` : undefined,
+    className
+  );
 
-    const pathItemClassName = classnames(
-      'slds-tabs_path__item',
-      `slds-is-${type}`,
-      className
-    );
+  const tabIndex = type === 'current' ? 0 : -1;
+  const completedText = completedTitle || 'Stage Complete';
 
-    const tabIndex = type === 'current' ? 0 : -1;
-    const completedText = completedTitle || 'Stage Complete';
+  return (
+    <li className={pathItemClassName} role='presentation'>
+      <a
+        className='slds-tabs_path__link'
+        aria-selected='false'
+        tabIndex={tabIndex}
+        role='tab'
+        aria-live='assertive'
+        onClick={onItemClick}
+      >
+        <span className='slds-tabs_path__stage'>
+          <Icon category='utility' icon='check' size='x-small' />
+          {type === 'complete' ? (
+            <span className='slds-assistive-text'>{completedText}</span>
+          ) : null}
+        </span>
+        <span className='slds-tabs_path__title'>{title}</span>
+      </a>
+    </li>
+  );
+};
 
-    return (
-      <li className={pathItemClassName} role='presentation'>
-        <a
-          className='slds-tabs_path__link'
-          aria-selected='false'
-          tabIndex={tabIndex}
-          role='tab'
-          aria-live='assertive'
-          onClick={this.onItemClick.bind(this, eventKey)}
-        >
-          <span className='slds-tabs_path__stage'>
-            <Icon category='utility' icon='check' size='x-small' />
-            {type === 'complete' ? (
-              <span className='slds-assistive-text'>{completedText}</span>
-            ) : null}
-          </span>
-          <span className='slds-tabs_path__title'>{title}</span>
-        </a>
-      </li>
-    );
-  }
-}
-
+/**
+ *
+ */
 export type SalesPathProps = {
   className?: string;
-  defaultActiveKey?: any;
-  activeKey?: any;
-  onSelect?: (itemKey: any) => void;
+  activeKey?: SalesPathKey;
+  defaultActiveKey?: SalesPathKey;
+  onSelect?: (itemKey: SalesPathKey) => void;
 };
 
-export type SalesPathState = {
-  activeKey?: any;
-};
+/**
+ *
+ */
+export const SalesPath: FC<SalesPathProps> = (props) => {
+  const {
+    activeKey: activeKey_,
+    defaultActiveKey,
+    className,
+    children,
+    onSelect: onSelect_,
+  } = props;
+  const [activeKey, setActiveKey] = useControlledValue(
+    activeKey_,
+    defaultActiveKey ?? null
+  );
+  const salesPathClassNames = classnames(className, 'slds-tabs_path');
 
-export class SalesPath extends React.Component<SalesPathProps, SalesPathState> {
-  static PathItem = PathItem;
+  const onSelect = useCallback(
+    (itemKey: SalesPathKey) => {
+      onSelect_?.(itemKey);
+      setActiveKey(itemKey);
+    },
+    [onSelect_, setActiveKey]
+  );
 
-  constructor(props: Readonly<SalesPathProps>) {
-    super(props);
-    this.state = {};
-
-    this.onItemClick = this.onItemClick.bind(this);
-  }
-
-  onItemClick(itemKey: any) {
-    if (this.props.onSelect) {
-      this.props.onSelect(itemKey);
+  let activeIdx = -1;
+  React.Children.forEach(children, (child, idx) => {
+    if (React.isValidElement(child)) {
+      const { eventKey } = child.props as unknown as {
+        eventKey?: SalesPathKey;
+      };
+      if (eventKey != null && eventKey === activeKey) {
+        activeIdx = idx;
+      }
     }
-    // Uncontrolled
-    this.setState({ activeKey: itemKey });
-  }
+  });
 
-  renderSalesPath(activeKey: any, paths: any) {
-    let typeTracker = -1;
+  const handlers = useMemo(() => ({ onSelect }), [onSelect]);
 
-    return React.Children.map(paths, (path) => {
-      const { eventKey, type, ...props } = path.props;
-      const isActive = eventKey === activeKey;
+  return (
+    <div className={salesPathClassNames} role='application tablist'>
+      <ul className='slds-tabs_path__nav' role='presentation'>
+        <SalesPathHandlersContext.Provider value={handlers}>
+          {React.Children.map(children, (child, idx) => {
+            const evaluatedType =
+              idx === activeIdx
+                ? 'current'
+                : idx < activeIdx
+                ? 'complete'
+                : 'incomplete';
+            return (
+              <SalesPathTypeContext.Provider value={evaluatedType}>
+                {child}
+              </SalesPathTypeContext.Provider>
+            );
+          })}
+        </SalesPathHandlersContext.Provider>
+      </ul>
+    </div>
+  );
+};
 
-      typeTracker = isActive ? 0 : typeTracker >= 0 ? 1 : -1;
-
-      const evaluatedType =
-        type ||
-        (isActive ? 'current' : typeTracker === -1 ? 'complete' : 'incomplete');
-
-      return (
-        <PathItem
-          eventKey={eventKey}
-          type={evaluatedType}
-          onSelect={this.onItemClick}
-          {...props}
-        />
-      );
-    });
-  }
-
-  render() {
-    const { className, children } = this.props;
-    const activeKey =
-      this.props.activeKey ||
-      this.state.activeKey ||
-      this.props.defaultActiveKey;
-
-    const salesPathClassNames = classnames(className, 'slds-tabs_path');
-    return (
-      <div className={salesPathClassNames} role='application tablist'>
-        <ul className='slds-tabs_path__nav' role='presentation'>
-          {this.renderSalesPath(activeKey, children)}
-        </ul>
-      </div>
-    );
-  }
-}
+(SalesPath as unknown as { PathItem: typeof SalesPathItem }).PathItem =
+  SalesPathItem;
