2f248a400f0fcf874eb8ee6c80d8c41d306af83b
msmx-mnakagawa <mnakagawa@mashmatrix.com>
2025-05-30 13:20:15 +0900

    (Textarea): improve a11y

--- src/scripts/Textarea.tsx
+++ src/scripts/Textarea.tsx
@@ -1,4 +1,5 @@
 import React, {
+  useId,
   ChangeEvent,
   Ref,
   TextareaHTMLAttributes,
@@ -51,6 +52,7 @@ export const Textarea = createFC<TextareaProps, { isFormElement: boolean }>(
       prevValueRef.current = e.target.value;
     });
     const { isFieldSetColumn } = useContext(FieldSetColumnContext);
+    const errorId = useId();
     const taClassNames = classnames(className, 'slds-textarea');
     const textareaElem = readOnly ? (
       <Text type='regular' category='body'>
@@ -63,14 +65,16 @@ export const Textarea = createFC<TextareaProps, { isFormElement: boolean }>(
         className={taClassNames}
         {...rprops}
         onChange={onChange}
+        aria-describedby={error ? errorId : undefined}
       />
     );
     if (isFieldSetColumn || label || required || error || cols) {
       const formElemProps = {
-        id,
+        htmlFor: id,
         label,
         required,
         error,
+        errorId,
         cols,
         elementRef,
         readOnly,
