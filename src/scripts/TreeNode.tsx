4589d04fcb00dabeab72137ac166269e7112c136
msmx-mnakagawa <mnakagawa@mashmatrix.com>
2025-06-11 16:09:59 +0900

    Revert "(TreeNode): update markups"
    This reverts commit 4d3af82f713de933674a04e7cac58833140b24fb.

--- src/scripts/TreeNode.tsx
+++ src/scripts/TreeNode.tsx
@@ -84,37 +84,26 @@ const TreeNodeItem: FC<TreeNodeProps & { icon?: string }> = (props) => {
       )}
       {!leaf ? (
         <Button
-          className='slds-m-right_x-small'
+          className='slds-m-right_small'
           aria-controls=''
           aria-hidden='true'
           tabIndex={-1}
           type='icon-bare'
           icon={icon}
-          iconSize='x-small'
+          iconSize='small'
           onClick={onToggle}
           title={typeof label === 'string' ? `Expand ${label}` : undefined}
           // Prevent focus loss during loading by keeping the toggle button in the DOM with opacity set to 0.
           style={loading ? { opacity: 0, pointerEvents: 'none' } : undefined}
         />
       ) : null}
-      <span className='slds-has-flexi-truncate'>
-        {onLabelClick ? (
-          <a
-            className='slds-tree__item-label slds-truncate'
-            tabIndex={-1}
-            onClick={onLabelClick}
-          >
-            {ItemRender ? <ItemRender {...props} /> : label}
-          </a>
-        ) : (
-          <span
-            className='slds-tree__item-label slds-truncate'
-            title={typeof label === 'string' ? label : undefined}
-          >
-            {ItemRender ? <ItemRender {...props} /> : label}
-          </span>
-        )}
-      </span>
+      <a
+        className='slds-truncate'
+        tabIndex={-1}
+        onClick={onLabelClick}
+      >
+        {ItemRender ? <ItemRender {...props} /> : label}
+      </a>
       {leaf ? children : null}
     </div>
   );
