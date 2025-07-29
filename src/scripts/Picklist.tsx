664cfbc95b77271f59a3cbae82d78ff04e26fefe
msmx-mnakagawa <mnakagawa@mashmatrix.com>
2025-07-29 14:54:42 +0900

    Revert "(Picklist): remove a style disabling a focus outline"
    This reverts commit 42543c59d13fa01fee1873c6906e65180e566a6d.

--- src/scripts/Picklist.tsx
+++ src/scripts/Picklist.tsx
@@ -191,6 +191,7 @@ const PicklistContext = createContext<{
 function useInitComponentStyle() {
   useEffect(() => {
     registerStyle('picklist', [
+      ['.react-picklist-input:focus-visible', '{ outline: none; }'],
       ['.react-picklist-input:not(:disabled)', '{ cursor: pointer; }'],
     ]);
   }, []);
